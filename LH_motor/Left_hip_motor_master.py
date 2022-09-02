90831e9197ad6cb581c868e481027dc565bb5291
wilson455039 <70258492+wilson455039@users.noreply.github.com>
2022-09-02 10:45:57 +1000

    update LH_motor testing code

--- LH_motor/Left_hip_motor_master.py
+++ LH_motor/Left_hip_motor_master.py
@@ -1,6 +1,12 @@
 import canopen
 import time
 
+# enable the simulated Left hip motor:
+# sudo modprobe vcan
+# sudo ip link add dev vcan0 type vcan
+# sudo ip link set up vcan0
+# candump -t d vcan0 #absoluted time:(-t a); related time:(-t d) 
+
 # construct a CAN network
 network = canopen.Network()
 
