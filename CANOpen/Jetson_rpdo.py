57c283228df599eebd200495aa4c51a870d5b315
Wilson <70258492+wilson455039@users.noreply.github.com>
2022-09-06 01:43:07 +1000

    Add model input circular buffer function

--- CANOpen/Jetson_rpdo.py
+++ CANOpen/Jetson_rpdo.py
@@ -29,74 +29,84 @@ Jetson_66.tpdo.read()
 
 print("CAN BUS configuration completed, waiting RPDO...")
 
-# #convert hex signed 2's complement to int
-# def hex2dec(hex_value, datatype): # hex_value = object of rpdo in str matrix, datatype = desired decimal data type
-#     if datatype == 'int32': # motor position and velocity data type
-#         num_bit = 4
-#     if datatype == 'int16': # motor torque data type
-#         num_bit = 2
-#     # if datatype == 'int32': # crutch force sensor data type
-#     #     num_bit = 2
     
-#     hex_seg = [0]*num_bit
-#     for i in range(num_bit):
-#         hex_seg[i] = hex_str[0:2]
+Left_unsigned16bit_raw = [0]*12
+Right_unsigned16bit_raw = [0]*12
+Left_crutch_data = [0]*6
+Right_crutch_data = [0]*6
+isPDOreceived = [0]*12
 
-#     return 
-
-# #convert int to hex signed 2's complement
-# def dec2hex(dec_value, datatype): # hex_value = object of rpdo in str matrix, datatype = desired decimal data type
-#     if datatype == 'int32': # motor position and velocity data type
-#         num_bit = 4
-#     if datatype == 'int16': # motor torque data type
-#         num_bit = 2
-#     # if datatype == 'int32': # crutch force sensor data type
-#     #     num_bit = 2
-
-#     valueInByte = (dec_value).to_bytes(num_bit, byteorder="little", signed=True)
-#     # hexadecimal_result = format(dec_value, "03X")
-#     # hex_str = hexadecimal_result.zfill(num_bit*2)
-#     hex_seg = [0]*num_bit
-#     for i in range(num_bit):
-#         hex_seg[i] = valueInByte[0+i:1+i]
-#     return hex_seg
 
 def process_rpdo(message): # "message" type: 'canopen.pdo.base.Map'; var type: 'canopen.pdo.base.Variable'  
-    print('%s received' % message.name)
+    # print('%s received' % message.name)
     cob_id = str(hex(message.cob_id))
     # Add crutch sensor pdo criteria by message.cob_id
 
     # (message.arbitration_id, message.data) 'Map' object has no attribute 'arbitration_id' ???
     splited_hex = [0]*int((len(message)))
     i = 0
-    
+    global isPDOreceived
+    global LH_position
+    global LK_position
+    global RH_position
+    global RK_position
+    global LH_velocity
+    global LK_velocity
+    global RH_velocity
+    global RK_velocity
+    global LH_torque
+    global LK_torque
+    global RH_torque
+    global RK_torque
+
     for var in message:
         splited_hex[i] = var.raw
         i = i+1
         # print('%s = %d' % (var.name, var.raw)) # var.name = str; var.raw = int(Raw representation of the object.)
         # print(type(splited_hex[0]))
-    if cob_id[2:4] == '28': # if rpdo is 0x2-- , split msg into position and velocity
+    # Left Hip Motor   
+    if cob_id[2:5] == '281': # if rpdo is 0x2-- , split msg into position and velocity
         # split list > create bytes class > convert bytes to int in little-endian
-        position = int.from_bytes(bytes(splited_hex[0:4]), byteorder='little', signed=True) 
-        velocity = int.from_bytes(bytes(splited_hex[4:8]), byteorder='little', signed=True)
-        position_circular.append(position)
-        velocity_circular.append(velocity)
-        print("position =", position)
-        print("velovity =", velocity)
-        print("position circular=", position_circular)
-        print("velovity circular=", velocity_circular)   
-    elif cob_id[2:4] == '38': # if rpdo is 0x3--, set denominator to 1 to extract all msg as torque
-        torque = int.from_bytes(bytes(splited_hex), byteorder='little', signed=True)
-        torque_circular.append(torque)
-        print("torque =", torque)
-        print("torque circular=", torque_circular)
+        LH_position = int.from_bytes(bytes(splited_hex[0:4]), byteorder='little', signed=True) 
+        LH_velocity = int.from_bytes(bytes(splited_hex[4:8]), byteorder='little', signed=True)
+        isPDOreceived[0] = 1
+    elif cob_id[2:5] == '381': # if rpdo is 0x3--, set denominator to 1 to extract all msg as torque
+        LH_torque = int.from_bytes(bytes(splited_hex), byteorder='little', signed=True)
+        isPDOreceived[1] = 1
+    # Left Knee Motor
+    elif cob_id[2:5] == '282':
+        LK_position = int.from_bytes(bytes(splited_hex[0:4]), byteorder='little', signed=True) 
+        LK_velocity = int.from_bytes(bytes(splited_hex[4:8]), byteorder='little', signed=True)
+        isPDOreceived[2] = 1
+    elif cob_id[2:5] == '382':
+        LK_torque = int.from_bytes(bytes(splited_hex), byteorder='little', signed=True)
+        isPDOreceived[3] = 1
+    # Right Hip Motor
+    elif cob_id[2:5] == '283':
+        RH_position = int.from_bytes(bytes(splited_hex[0:4]), byteorder='little', signed=True) 
+        RH_velocity = int.from_bytes(bytes(splited_hex[4:8]), byteorder='little', signed=True)
+        isPDOreceived[4] = 1
+    elif cob_id[2:5] == '383':
+        RH_torque = int.from_bytes(bytes(splited_hex), byteorder='little', signed=True)
+        isPDOreceived[5] = 1
+    # Right Knee Motor
+    elif cob_id[2:5] == '284':
+        RK_position = int.from_bytes(bytes(splited_hex[0:4]), byteorder='little', signed=True) 
+        RK_velocity = int.from_bytes(bytes(splited_hex[4:8]), byteorder='little', signed=True)
+        isPDOreceived[6] = 1
+    elif cob_id[2:5] == '384':
+        RK_torque = int.from_bytes(bytes(splited_hex), byteorder='little', signed=True)
+        isPDOreceived[7] = 1
+
     # Config crutch sensor data convertion
     elif cob_id[2:4] == 'f1':
         for i in range(7):
             Left_unsigned16bit_raw[i] = splited_hex[i+1]
+        isPDOreceived[8] = 1
     elif cob_id[2:4] == 'f9':
         for i in range(7):
             Right_unsigned16bit_raw[i] = splited_hex[i+1]
+        isPDOreceived[9] = 1
     elif cob_id[2:4] == 'f2':
         for i in range(5):
             Left_unsigned16bit_raw[i+7] = splited_hex[i]
@@ -108,7 +118,8 @@ def process_rpdo(message): # "message" type: 'canopen.pdo.base.Map'; var type: '
                 Left_crutch_data[i] = Left_crutch_data[i]/50
             elif(i>=3):
                 Left_crutch_data[i] = Left_crutch_data[i]/2000
-        print("Left_crutch_data",Left_crutch_data)
+        isPDOreceived[10] = 1
+        # print("Left_crutch_data",Left_crutch_data)
     elif cob_id[2:4] == 'fa':
         for i in range(5):
             Right_unsigned16bit_raw[i+7] = splited_hex[i]
@@ -120,16 +131,35 @@ def process_rpdo(message): # "message" type: 'canopen.pdo.base.Map'; var type: '
                 Right_crutch_data[i] = Right_crutch_data[i]/50
             elif(i>=3):
                 Right_crutch_data[i] = Right_crutch_data[i]/2000
-        print("Right_crutch_data",Right_crutch_data)
-    
-Left_unsigned16bit_raw = [0]*12
-Right_unsigned16bit_raw = [0]*12
-Left_crutch_data = [0]*6
-Right_crutch_data = [0]*6
-position_circular = CircularBuffer.circularlist(4)
-velocity_circular = CircularBuffer.circularlist(4)
-torque_circular = CircularBuffer.circularlist(4)
-input_data = CircularBuffer.circularlist(4)
+        isPDOreceived[11] = 1
+        # print("Right_crutch_data",Right_crutch_data)
+
+    if(isPDOreceived[0]==1 and isPDOreceived[1]==1 and isPDOreceived[2]==1 and isPDOreceived[3]==1 and isPDOreceived[4]==1
+     and isPDOreceived[5]==1 and isPDOreceived[6]==1 and isPDOreceived[7]==1 and isPDOreceived[8]==1 and isPDOreceived[9]==1
+     and isPDOreceived[10]==1 and isPDOreceived[11]==1):
+        for i in range(6):
+            model_input_circular.append(Left_crutch_data[i])
+        for i in range(6):
+            model_input_circular.append(Right_crutch_data[i])
+        model_input_circular.append(LH_position)
+        model_input_circular.append(LK_position)
+        model_input_circular.append(RH_position)
+        model_input_circular.append(RK_position)
+        model_input_circular.append(LH_velocity)
+        model_input_circular.append(LK_velocity)
+        model_input_circular.append(RH_velocity)
+        model_input_circular.append(RK_velocity)
+        model_input_circular.append(LH_torque)
+        model_input_circular.append(LK_torque)
+        model_input_circular.append(RH_torque)
+        model_input_circular.append(RK_torque)
+        isPDOreceived = [0]*12
+        print("model_input_circular=", model_input_circular)
+
+
+
+# print("isPDOreceived",isPDOreceived)
+model_input_circular  = CircularBuffer.circularlist(24)
 
 for i in range(1,num_rpdo):
     Jetson_66.rpdo[i].add_callback(process_rpdo)
