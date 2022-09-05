6f1ced536848c932290d02691abb04ab1b4006d3
Wilson <70258492+wilson455039@users.noreply.github.com>
2022-09-06 01:42:05 +1000

    clean up code structure

--- Simulator/Exo_test_tpdo.py
+++ Simulator/Exo_test_tpdo.py
@@ -87,11 +87,120 @@ def crutch2can(value, datatype): #convert a float to 2 one bye can message force
         hex_seg[i] = int.from_bytes(valueInByte[0+i:1+i], byteorder="big")
     return hex_seg
 
+def config_all(value):
+    splited_value_1 = 0
+    splited_value_2 = 0
+    # pdo 1
+    splited_value_1 = dec2hex(value, 'int32')
+    splited_value_2 = dec2hex(value-16, 'int32')
+    # print("splited_value_1 = ",splited_value_1)
+    # print("splited_value_2 = ",splited_value_2)
+    Exo_test.tpdo[1][0x200F].raw = splited_value_1[0]
+    Exo_test.tpdo[1][0x2010].raw = splited_value_1[1]
+    Exo_test.tpdo[1][0x2011].raw = splited_value_1[2]
+    Exo_test.tpdo[1][0x2012].raw = splited_value_1[3]
+    Exo_test.tpdo[1][0x2013].raw = splited_value_2[0]
+    Exo_test.tpdo[1][0x2014].raw = splited_value_2[1]
+    Exo_test.tpdo[1][0x2015].raw = splited_value_2[2]
+    Exo_test.tpdo[1][0x2016].raw = splited_value_2[3]
+    # pdo 2
+    splited_value_1 = dec2hex(value, 'int16')
+    Exo_test.tpdo[2][0x2017].raw = splited_value_1[0]
+    Exo_test.tpdo[2][0x2018].raw = splited_value_1[1]
+    # pdo 3
+    splited_value_1 = dec2hex(value, 'int32')
+    splited_value_2 = dec2hex(value-16, 'int32')
+    Exo_test.tpdo[3][0x2019].raw = splited_value_1[0]
+    Exo_test.tpdo[3][0x201A].raw = splited_value_1[1]
+    Exo_test.tpdo[3][0x201B].raw = splited_value_1[2]
+    Exo_test.tpdo[3][0x201C].raw = splited_value_1[3]
+    Exo_test.tpdo[3][0x201D].raw = splited_value_2[0]
+    Exo_test.tpdo[3][0x201E].raw = splited_value_2[1]
+    Exo_test.tpdo[3][0x201F].raw = splited_value_2[2]
+    Exo_test.tpdo[3][0x2020].raw = splited_value_2[3]
+    # pdo 4
+    splited_value_1 = dec2hex(value, 'int16')
+    Exo_test.tpdo[4][0x2021].raw = splited_value_1[0]
+    Exo_test.tpdo[4][0x2022].raw = splited_value_1[1]
+    # pdo 5
+    splited_value_1 = dec2hex(value, 'int32')
+    splited_value_2 = dec2hex(value-16, 'int32')
+    Exo_test.tpdo[5][0x2023].raw = splited_value_1[0]
+    Exo_test.tpdo[5][0x2024].raw = splited_value_1[1]
+    Exo_test.tpdo[5][0x2025].raw = splited_value_1[2]
+    Exo_test.tpdo[5][0x2026].raw = splited_value_1[3]
+    Exo_test.tpdo[5][0x2027].raw = splited_value_2[0]
+    Exo_test.tpdo[5][0x2028].raw = splited_value_2[1]
+    Exo_test.tpdo[5][0x2029].raw = splited_value_2[2]
+    Exo_test.tpdo[5][0x202A].raw = splited_value_2[3]
+    # pdo 6
+    splited_value_1 = dec2hex(value, 'int16')
+    Exo_test.tpdo[6][0x202B].raw = splited_value_1[0]
+    Exo_test.tpdo[6][0x202C].raw = splited_value_1[1]
+    # pdo 7
+    splited_value_1 = dec2hex(value, 'int32')
+    splited_value_2 = dec2hex(value-16, 'int32')
+    Exo_test.tpdo[7][0x202D].raw = splited_value_1[0]
+    Exo_test.tpdo[7][0x202E].raw = splited_value_1[1]
+    Exo_test.tpdo[7][0x202F].raw = splited_value_1[2]
+    Exo_test.tpdo[7][0x2030].raw = splited_value_1[3]
+    Exo_test.tpdo[7][0x2031].raw = splited_value_2[0]
+    Exo_test.tpdo[7][0x2032].raw = splited_value_2[1]
+    Exo_test.tpdo[7][0x2033].raw = splited_value_2[2]
+    Exo_test.tpdo[7][0x2034].raw = splited_value_2[3]
+    # pdo 8
+    splited_value_1 = dec2hex(value, 'int16')
+    Exo_test.tpdo[8][0x2035].raw = splited_value_1[0]
+    Exo_test.tpdo[8][0x2036].raw = splited_value_1[1]
+    # pdo 9 & 10
+    #convert unsigned 16 bit to signed 16 bit: signed int16 = unsigned int16 - 2^16
+    crutch_input_force_data = value/50
+    crutch_input_torque_data = value/2000
+    force_can = crutch2can(crutch_input_force_data, "force")
+    torque_can = crutch2can(crutch_input_torque_data, "torque")
+    Exo_test.tpdo[9][0x2037].raw = 0
+    Exo_test.tpdo[9][0x2038].raw = force_can[0]
+    Exo_test.tpdo[9][0x2039].raw = force_can[1]
+    Exo_test.tpdo[9][0x203A].raw = force_can[0]
+    Exo_test.tpdo[9][0x203B].raw = force_can[1]
+    Exo_test.tpdo[9][0x203C].raw = force_can[0]
+    Exo_test.tpdo[9][0x203D].raw = force_can[1]
+    Exo_test.tpdo[9][0x203E].raw = torque_can[0]
+    Exo_test.tpdo[10][0x203F].raw = torque_can[1]
+    Exo_test.tpdo[10][0x2040].raw = torque_can[0]
+    Exo_test.tpdo[10][0x2041].raw = torque_can[1]
+    Exo_test.tpdo[10][0x2042].raw = torque_can[0]
+    Exo_test.tpdo[10][0x2043].raw = torque_can[1]
+    Exo_test.tpdo[10][0x2044].raw = 0
+    Exo_test.tpdo[10][0x2045].raw = 0
+    Exo_test.tpdo[10][0x2046].raw = 0
+    # pdo 11 & 12
+    #convert unsigned 16 bit to signed 16 bit: signed int16 = unsigned int16 - 2^16
+    crutch_input_force_data = value/50
+    crutch_input_torque_data = value/2000
+    force_can = crutch2can(crutch_input_force_data, "force")
+    torque_can = crutch2can(crutch_input_torque_data, "torque")
+    Exo_test.tpdo[11][0x2047].raw = 0
+    Exo_test.tpdo[11][0x2048].raw = force_can[0]
+    Exo_test.tpdo[11][0x2049].raw = force_can[1]
+    Exo_test.tpdo[11][0x204A].raw = force_can[0]
+    Exo_test.tpdo[11][0x204B].raw = force_can[1]
+    Exo_test.tpdo[11][0x204C].raw = force_can[0]
+    Exo_test.tpdo[11][0x204D].raw = force_can[1]
+    Exo_test.tpdo[11][0x204E].raw = torque_can[0]
+    Exo_test.tpdo[12][0x204F].raw = torque_can[1]
+    Exo_test.tpdo[12][0x2050].raw = torque_can[0]
+    Exo_test.tpdo[12][0x2051].raw = torque_can[1]
+    Exo_test.tpdo[12][0x2052].raw = torque_can[0]
+    Exo_test.tpdo[12][0x2053].raw = torque_can[1]
+    Exo_test.tpdo[12][0x2054].raw = 0
+    Exo_test.tpdo[12][0x2055].raw = 0
+    Exo_test.tpdo[12][0x2056].raw = 0
+
 # test sending PDO to set output
 print("test sending PDO to set output")
 
-
-test_id = 9
+test_id = 0
 # test list: (ignore this list)
 # id = 0: No one
 # id = 1: All
@@ -106,128 +215,48 @@ test_id = 9
 # id = 10: Crutch UI Controller
 
 
-
-
 def transmit_pdo(id, value):
     splited_value_1 = 0
     splited_value_2 = 0
     print("Testing TPDO:",id )
-    if(id==1):
-        splited_value_1 = dec2hex(write_data, 'int32')
-        splited_value_2 = dec2hex(write_data-16, 'int32')
-        Exo_test.tpdo[id][0x200F].raw = splited_value_1[0]
-        Exo_test.tpdo[id][0x2010].raw = splited_value_1[1]
-        Exo_test.tpdo[id][0x2011].raw = splited_value_1[2]
-        Exo_test.tpdo[id][0x2012].raw = splited_value_1[3]
-        Exo_test.tpdo[id][0x2013].raw = splited_value_2[0]
-        Exo_test.tpdo[id][0x2014].raw = splited_value_2[1]
-        Exo_test.tpdo[id][0x2015].raw = splited_value_2[2]
-        Exo_test.tpdo[id][0x2016].raw = splited_value_2[3]
+    config_all(value)
+    if(id==0): # transmit ALL PDO
+        for i in range(1,13):
+            Exo_test.tpdo[i].transmit()
+    elif(id==1):
         print("TPDO Transmit value = ", write_data, write_data-16)
+        Exo_test.tpdo[id].transmit()
     elif(id==2):
-        splited_value_1 = dec2hex(write_data, 'int16')
-        Exo_test.tpdo[id][0x2017].raw = splited_value_1[0]
-        Exo_test.tpdo[id][0x2018].raw = splited_value_1[1]
         print("TPDO Transmit value = ", write_data)
+        Exo_test.tpdo[id].transmit()
     elif(id==3):
-        splited_value_1 = dec2hex(write_data, 'int32')
-        splited_value_2 = dec2hex(write_data-16, 'int32')
-        Exo_test.tpdo[id][0x2019].raw = splited_value_1[0]
-        Exo_test.tpdo[id][0x201A].raw = splited_value_1[1]
-        Exo_test.tpdo[id][0x201B].raw = splited_value_1[2]
-        Exo_test.tpdo[id][0x201C].raw = splited_value_1[3]
-        Exo_test.tpdo[id][0x201D].raw = splited_value_2[0]
-        Exo_test.tpdo[id][0x201E].raw = splited_value_2[1]
-        Exo_test.tpdo[id][0x201F].raw = splited_value_2[2]
-        Exo_test.tpdo[id][0x2020].raw = splited_value_2[3]
         print("TPDO Transmit value = ", write_data, write_data-16)
+        Exo_test.tpdo[id].transmit()
     elif(id==4):
-        splited_value_1 = dec2hex(write_data, 'int16')
-        Exo_test.tpdo[id][0x2021].raw = splited_value_1[0]
-        Exo_test.tpdo[id][0x2022].raw = splited_value_1[1]
         print("TPDO Transmit value = ", write_data)
+        Exo_test.tpdo[id].transmit()
     elif(id==5):
-        splited_value_1 = dec2hex(write_data, 'int32')
-        splited_value_2 = dec2hex(write_data-16, 'int32')
-        Exo_test.tpdo[id][0x2023].raw = splited_value_1[0]
-        Exo_test.tpdo[id][0x2024].raw = splited_value_1[1]
-        Exo_test.tpdo[id][0x2025].raw = splited_value_1[2]
-        Exo_test.tpdo[id][0x2026].raw = splited_value_1[3]
-        Exo_test.tpdo[id][0x2027].raw = splited_value_2[0]
-        Exo_test.tpdo[id][0x2028].raw = splited_value_2[1]
-        Exo_test.tpdo[id][0x2029].raw = splited_value_2[2]
-        Exo_test.tpdo[id][0x202A].raw = splited_value_2[3]
         print("TPDO Transmit value = ", write_data, write_data-16)
+        Exo_test.tpdo[id].transmit()
     elif(id==6):
-        splited_value_1 = dec2hex(write_data, 'int16')
-        Exo_test.tpdo[id][0x202B].raw = splited_value_1[0]
-        Exo_test.tpdo[id][0x202C].raw = splited_value_1[1]
         print("TPDO Transmit value = ", write_data)
+        Exo_test.tpdo[id].transmit()
     elif(id==7):
-        splited_value_1 = dec2hex(write_data, 'int32')
-        splited_value_2 = dec2hex(write_data-16, 'int32')
-        Exo_test.tpdo[id][0x202D].raw = splited_value_1[0]
-        Exo_test.tpdo[id][0x202E].raw = splited_value_1[1]
-        Exo_test.tpdo[id][0x202F].raw = splited_value_1[2]
-        Exo_test.tpdo[id][0x2030].raw = splited_value_1[3]
-        Exo_test.tpdo[id][0x2031].raw = splited_value_2[0]
-        Exo_test.tpdo[id][0x2032].raw = splited_value_2[1]
-        Exo_test.tpdo[id][0x2033].raw = splited_value_2[2]
-        Exo_test.tpdo[id][0x2034].raw = splited_value_2[3]
         print("TPDO Transmit value = ", write_data, write_data-16)
+        Exo_test.tpdo[id].transmit()
     elif(id==8):
-        splited_value_1 = dec2hex(write_data, 'int16')
-        Exo_test.tpdo[id][0x2035].raw = splited_value_1[0]
-        Exo_test.tpdo[id][0x2036].raw = splited_value_1[1]
         print("TPDO Transmit value = ", write_data)
+        Exo_test.tpdo[id].transmit()
     elif(id==9):
-        #convert unsigned 16 bit to signed 16 bit: signed int16 = unsigned int16 - 2^16
-        crutch_input_force_data = write_data/50
-        crutch_input_torque_data = write_data/2000
-        force_can = crutch2can(crutch_input_force_data, "force")
-        torque_can = crutch2can(crutch_input_torque_data, "torque")
-        Exo_test.tpdo[9][0x2037].raw = 0
-        Exo_test.tpdo[9][0x2038].raw = force_can[0]
-        Exo_test.tpdo[9][0x2039].raw = force_can[1]
-        Exo_test.tpdo[9][0x203A].raw = force_can[0]
-        Exo_test.tpdo[9][0x203B].raw = force_can[1]
-        Exo_test.tpdo[9][0x203C].raw = force_can[0]
-        Exo_test.tpdo[9][0x203D].raw = force_can[1]
-        Exo_test.tpdo[9][0x203E].raw = torque_can[0]
-        Exo_test.tpdo[10][0x203F].raw = torque_can[1]
-        Exo_test.tpdo[10][0x2040].raw = torque_can[0]
-        Exo_test.tpdo[10][0x2041].raw = torque_can[1]
-        Exo_test.tpdo[10][0x2042].raw = torque_can[0]
-        Exo_test.tpdo[10][0x2043].raw = torque_can[1]
-        Exo_test.tpdo[10][0x2044].raw = 0
-        Exo_test.tpdo[10][0x2045].raw = 0
-        Exo_test.tpdo[10][0x2046].raw = 0
-        print("TPDO Transmit value = ", crutch_input_force_data, crutch_input_torque_data)
+        print("TPDO Transmit value = ", write_data/50, write_data/2000)
+        Exo_test.tpdo[id].transmit()
+        Exo_test.tpdo[id+1].transmit()
     elif(id==10):
         print("Testing TPDO:",id)
     elif(id==11):
-        #convert unsigned 16 bit to signed 16 bit: signed int16 = unsigned int16 - 2^16
-        crutch_input_force_data = write_data/50
-        crutch_input_torque_data = write_data/2000
-        force_can = crutch2can(crutch_input_force_data, "force")
-        torque_can = crutch2can(crutch_input_torque_data, "torque")
-        Exo_test.tpdo[11][0x2047].raw = 0
-        Exo_test.tpdo[11][0x2048].raw = force_can[0]
-        Exo_test.tpdo[11][0x2049].raw = force_can[1]
-        Exo_test.tpdo[11][0x204A].raw = force_can[0]
-        Exo_test.tpdo[11][0x204B].raw = force_can[1]
-        Exo_test.tpdo[11][0x204C].raw = force_can[0]
-        Exo_test.tpdo[11][0x204D].raw = force_can[1]
-        Exo_test.tpdo[11][0x204E].raw = torque_can[0]
-        Exo_test.tpdo[12][0x204F].raw = torque_can[1]
-        Exo_test.tpdo[12][0x2050].raw = torque_can[0]
-        Exo_test.tpdo[12][0x2051].raw = torque_can[1]
-        Exo_test.tpdo[12][0x2052].raw = torque_can[0]
-        Exo_test.tpdo[12][0x2053].raw = torque_can[1]
-        Exo_test.tpdo[12][0x2054].raw = 0
-        Exo_test.tpdo[12][0x2055].raw = 0
-        Exo_test.tpdo[12][0x2056].raw = 0
-        print("TPDO Transmit value = ", crutch_input_force_data, crutch_input_torque_data)
+        print("TPDO Transmit value = ", write_data/50, write_data/2000)
+        Exo_test.tpdo[id].transmit()
+        Exo_test.tpdo[id+1].transmit()
     elif(id==12):
         print("Testing TPDO:",id)
     elif(id==13):
@@ -243,9 +272,6 @@ def transmit_pdo(id, value):
     elif(id==18):
         print("Testing TPDO:",id)
     
-    Exo_test.tpdo[id].transmit()
-    if(id>=9):
-        Exo_test.tpdo[id+1].transmit()
     
     
 
@@ -281,7 +307,7 @@ try:
 
         # Exo_test.tpdo[1].transmit()
         # print("write output value = {}".format(Exo_test.tpdo[1][0x2000].raw))
-        time.sleep(1)
+        time.sleep(0.1)
 except KeyboardInterrupt:
     print("exit from sending PDO to Jetson")
 
